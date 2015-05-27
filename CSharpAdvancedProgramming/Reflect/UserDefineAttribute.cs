0980c1be3ffe2b80c0900b88caee97b3ac81e8c8
sanyeMen <liu610721@gmail.com>
2015-05-27 13:43:04 +0800

    添加自定义特性

---
+++ CSharpAdvancedProgramming/Reflect/UserDefineAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace CSharpAdvancedProgramming.Reflect
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple=true, Inherited=true)]
+    public class UserDefineAttribute : Attribute
+    {
+
+        public UserDefineAttribute(DateTime lastmodify, string msg)
+        {
+            this.lastModify = lastmodify;
+            this.message = msg;
+        }
+
+        private DateTime lastModify;
+
+        public DateTime LastModify
+        {
+            get { return lastModify; }
+            set { this.lastModify = value; }
+        }
+
+        private string message;
+
+        public string Message
+        {
+            get { return message; }
+            set { this.message = value; }
+        }
+
+
+    }
+}
